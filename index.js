89a0a307339c478d4ead477a688f80f73dce5a6e
Arian Mahdavi <amahdavi9978@gmail.com>
2024-05-23 18:52:26 +0330

    Create a new function for add new contact.

---
+++ index.js
@@ -0,0 +1,18 @@
+import readline from "readline/promises";
+import { stdin as input, stdout as output } from "process";
+
+const rl = readline.createInterface({ input, output });
+const contactList = [];
+
+async function addNewContact() {
+  const firstName = await rl.question("Enter your firstname: ");
+  const lastName = await rl.question("Enter your lastname: ");
+
+  const newContact = {
+    id: contactList.length + 1,
+    firstName,
+    lastName,
+  };
+
+  contactList.push(addNewContact);
+}
