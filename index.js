812f697aa30aab01f557cf78f205f01929718927
Arian Mahdavi <amahdavi9978@gmail.com>
2024-05-23 19:03:38 +0330

    feat: added helper function and fix dev script in package json.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { stdin as input, stdout as output } from "process";
 const rl = readline.createInterface({ input, output });
 const contactList = [];
 
+console.log("--- contact-list App ---");
+
 async function addNewContact() {
   const firstName = await rl.question("Enter your firstname: ");
   const lastName = await rl.question("Enter your lastname: ");
@@ -28,3 +30,20 @@ function showContactList() {
 function quite() {
   rl.close();
 }
+
+async function help() {
+  console.log("a: add new contact\nl: show contact list\nq: quite");
+  const action = await rl.question("Enter your input: ");
+  if (action === "a") {
+    await addNewContact();
+  } else if (action === "l") {
+    showContactList();
+  } else {
+    quite();
+    return;
+  }
+
+  help();
+}
+
+await help();
